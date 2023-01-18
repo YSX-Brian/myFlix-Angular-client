eecbeff6dd5c33d4801a954e2a17164ed7d834b3
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-18 03:36:41 -0500

    Create login-form.component.ts

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+
+export class LoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<LoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      localStorage.setItem("token", result.token);
+      localStorage.setItem("user", result.user.Username);
+      this.dialogRef.close(); // This will close the modal on success!
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
