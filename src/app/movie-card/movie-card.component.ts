19b3bcdbf63c1d47b18f11f54e1feccdf9ec6cfa
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-19 23:44:45 -0500

    Update movie-card.component.ts

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-movie-card',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchApiData: UserRegistrationService) { }
 
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
 }
