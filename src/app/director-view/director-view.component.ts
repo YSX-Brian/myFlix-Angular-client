caa9741e613a05db5ad80abcad9b3a51b3aaf09e
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-21 14:28:20 -0500

    Create director-view.component.ts

---
+++ src/app/director-view/director-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-director-view',
+  templateUrl: './director-view.component.html',
+  styleUrls: ['./director-view.component.scss']
+})
+export class DirectorViewComponent {
+
+}
