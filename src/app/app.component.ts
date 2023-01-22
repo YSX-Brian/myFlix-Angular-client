6e7a8844f5d3d129fa3112040c1caa3ec2c57a4e
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-21 23:29:36 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,3 @@
-// import { Component } from '@angular/core';
-
-// import { MatDialog } from '@angular/material/dialog';
-// import { MovieCardComponent } from './movie-card/movie-card.component';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.scss']
-// })
-// export class AppComponent {
-//   title = 'myFlix-Angular-client';
-
-//   constructor(public dialog: MatDialog) { }
-//   // This is the function that will open the dialog when the signup button is clicked  
-
-//   openMoviesDialog(): void {
-//     this.dialog.open(MovieCardComponent, {
-//       width: '500px'
-//     });
-//   }
-
-// }
-
 import { Component } from '@angular/core';
 
 @Component({
