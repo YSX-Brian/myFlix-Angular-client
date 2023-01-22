c28495990af9066cad05295cc24f893a0d12f6ee
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-21 21:59:08 -0500

    create movie synopsis component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatDialog } from "@angular/material/dialog";
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: UserRegistrationService) { }
+  favoriteMovies: any[] = [];
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    private dialog: MatDialog,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
-    this.getMovies();
+    this.getMovies(),
+      this.getFavoriteMovies()
   }
 
   getMovies(): void {
@@ -21,4 +33,63 @@ export class MovieCardComponent {
       return this.movies;
     });
   }
+
+  getFavoriteMovies(): void {
+    this.fetchApiData.getFavoriteMovies().subscribe((resp: any) => {
+      this.favoriteMovies = resp;
+      console.log(this.favoriteMovies);
+    });
+  }
+
+  openGenreDialog(movie: any): void {
+    const { Name, Description } = movie.Genre;
+    this.dialog.open(GenreViewComponent, {
+      data: { Name, Description },
+      panelClass: "genre-dialog-background",
+      width: "400px",
+    });
+  }
+
+  openDirectorDialog(movie: any): void {
+    const { Name, Birth, Bio } = movie.Director;
+    this.dialog.open(DirectorViewComponent, {
+      data: { Name, Birth, Bio },
+      panelClass: "director-dialog-background",
+      width: "400px",
+    });
+  }
+
+  addFavoriteMovie(movieId: string): void {
+    this.fetchApiData.addFavorite(movieId).subscribe((resp: any) => {
+      this.getFavoriteMovies();
+    });
+  }
+
+  deleteFavoriteMovie(movieId: string): void {
+    this.fetchApiData.deleteFavorite(movieId).subscribe((resp: any) => {
+      this.getFavoriteMovies();
+    });
+  }
+
+  movieIsFavorite(movieId: string): boolean {
+    return this.favoriteMovies.includes(movieId);
+  }
+
+  toggleFavorite(movieId: string): void {
+    if (this.movieIsFavorite(movieId)) {
+      this.deleteFavoriteMovie(movieId);
+    } else {
+      this.addFavoriteMovie(movieId);
+    }
+  }
+
+  openMovieDetails(movie: any): void {
+    const { Title, Description } = movie;
+    this.dialog.open(MovieSynopsisComponent, {
+      data: { Title, Description },
+      panelClass: "synopsis-dialog-background",
+      width: "400px",
+    });
+  }
+
 }
